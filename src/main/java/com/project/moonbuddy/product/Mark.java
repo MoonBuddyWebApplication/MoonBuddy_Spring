98bf1ff98b25feac46e2f348908e9941a438faa6
supernovaMK <minki5336@naver.com>
2023-11-28 15:40:31 +0900

    [feat] Mark 완료!

--- src/main/java/com/project/moonbuddy/product/Mark.java
+++ src/main/java/com/project/moonbuddy/product/Mark.java
@@ -1,20 +1,22 @@
 package com.project.moonbuddy.product;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity(name="mark")
+@Setter
 public class Mark {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", insertable = false, updatable = false)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name="product_id")
     private Product product;
@@ -23,6 +25,13 @@ public class Mark {
     private String name;
     @Column(name="explanation",nullable = false)
     private String explanation;
+    @Column(name="image",nullable = false)
+    private String image;
 
+
+    @Override
+    public String toString() {
+        return "Mark{id=" + id + ", name='" + name + "', explanation='" + explanation + "', image='" + image + "'}";
+    }
 
 }
